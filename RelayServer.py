9311456d0e49ed9e2e0dc93666ebf029d321ce1c
Lexelius <Lexelius@hotmail.se>
2023-03-13 16:17:12 +0100

    Big pre-process commit
    Added pre-processing features:
        * cropping of diffraction patterns.
        * findig center of mass of the diffraction patterns,
            used for cropping if center has not been specified.
        * rebinning of diffraction pattern, and auto-generated mask.
    
    Changed how img and msg are saved into all_img and all_msg.

--- RelayServer.py
+++ RelayServer.py
@@ -1,4 +1,6 @@
 import json
+from typing import List, Any
+
 import zmq
 from bitshuffle import decompress_lz4
 from bitshuffle import compress_lz4
@@ -11,9 +13,12 @@ import struct
 
 import subprocess
 import sys
+from ptypy import utils as u
 
 
 class RelayServer(object):
+
+    recieved_image_indices: List[Any]
 
     def __init__(self, detector_address, motors_address, relay_address, simulate):
         print('start')
@@ -40,32 +45,39 @@ class RelayServer(object):
 
         # Initialize parameters
         self.running = True
-        self.latest_pos_index_received = -1  ## ToDo: check if these 2 should be in the beginning of check() instead
-        self.latest_det_index_received = -1
+        self.latest_pos_index_received = -1
+        self.latest_det_index_received = -1  # Counting every received image, assumes they come in the correct order and that no images are lost
         self.end_of_scan = False
         self.end_of_det_stream = False
         self.decomp_from_byte12 = detector_address.rsplit(':', 1)[0] == 'tcp://p-daq-cn-2'##'tcp://b-daq-node-2' ## used to determine how to decompress images
-        self.all_parts = []
-        self.all_info_json = []
-        self.all_info = []
-        self.all_img = []
-        self.all_msg = []
+        ### self.all_parts = []
+        ### self.all_info_json = []
+
+        ### self.all_info = []
+        self.all_img = {} #%#
+        self.all_msg = {} #%#
         self.Energy = None
-        self.recieved_image_indices = []
+        self.recieved_image_indices = []  # List of received frame indices, read from detector messages
         self.recieved_pos_indices = []
         self.latest_posimg_index_received = []
         self.nr_of_check_replies = 0
         self.energy_replied = False
+        self.load_replies = 0
+        self.init_params = {} #!#
+        self.do_crop = None #!#
+        self.center = None
+        self.newcenter = None
+        self.sendimg = []
 
     def run(self):
         i = -1
         j = -1
         while self.running:
             try:
-                try:
-                    self.latest_posimg_index_received.append([self.latest_pos_index_received, self.latest_det_index_received, self.recieved_image_indices[-1]])
-                except:
-                    pass
+                #!#try:
+                    #!#self.latest_posimg_index_received.append([self.latest_pos_index_received, self.latest_det_index_received, self.recieved_image_indices[-1]])
+                #!#except:
+                    #!#pass
                 print('')
                 print('receiving...')
                 # Find the first socket
@@ -81,20 +93,12 @@ class RelayServer(object):
                     i += 1
                     print(f'**** EIGER: {time.strftime("%H:%M:%S", time.localtime())}, {time.time()-self.t0} seconds')
                     parts = self.det_socket.recv_multipart(copy=False)
-                    self.all_parts.append(parts)
-                    ###info = json.loads(parts[0])##
-                    ###print(info)##
-                    ###header = json.loads(parts[0].bytes) # python dct
-                    ###print(header)
-                    info_json = parts[0].bytes
-                    self.all_info_json.append(info_json)
-                    info = json.loads(info_json)  ## makes a dict out of info_json
-                    self.all_info.append(info)
+                    info = json.loads(parts[0].bytes)  ## makes a dict out of info_json
+                    ### self.all_info.append(info)
                     print('info: ', info)
                     if info['htype'] == 'header':
                         print('********************* EIGER STARTING')
                     elif info['htype'] == 'image':
-                        print(f'Got diffraction pattern nr. {self.latest_det_index_received + 1}, decompressing...')
                         print(f'parts[1].buffer.nbytes = {parts[1].buffer.nbytes}')
                         # Temporary fix for dealing with bitshuffle weirdness
                         if self.decomp_from_byte12:
@@ -105,7 +109,8 @@ class RelayServer(object):
                         else:
                             ## This doesn't work when using modules at NanoMax, but works on conda locally, and with modules locally..
                             img = decompress_lz4(np.frombuffer(parts[1].buffer, dtype=np.dtype('uint8')), info['shape'], np.dtype(info['type']))
-                        self.all_img.append(img)
+                        #%#self.all_img.append(img)
+                        self.all_img[info['frame']] = img
                         self.recieved_image_indices.append(info['frame'])
                         self.latest_det_index_received += 1
                     elif info['htype'] == 'series_end':
@@ -116,19 +121,13 @@ class RelayServer(object):
                     print(f'\t**** CONTRAST: {time.strftime("%H:%M:%S", time.localtime())}, {time.time()-self.t0} seconds')
                     msg = self.pos_socket.recv_pyobj()
                     if msg['status'] == 'started':
-                        j += 1
-                        self.all_msg.append(msg)
                         print("\tmsg['status'] = motors started")
                         self.Energy = msg['snapshot']['energy']
                     elif msg['status'] == 'running':
                         j += 1
-                        self.all_msg.append(msg)
-                        self.latest_pos_index_received += 1
-                        self.recieved_pos_indices.append(j)
-                        print("\tmsg['status'] = motors running, position nr. %d recieved" % self.latest_pos_index_received)
+                        #%#self.all_msg.append(msg)
+                        self.all_msg[j] = msg  #%#
                     elif msg['status'] == 'finished':
-                        j += 1
-                        self.all_msg.append(msg)
                         self.end_of_scan = True
                         print("\tmsg['status'] = motors finished")
                         print('\n\n')
@@ -136,19 +135,27 @@ class RelayServer(object):
                         print('Message was not important')
 
                     for key, value in msg.items():
-                        print(f'\t {str(key)} :\t {str(value)}')
+                        print(f'\t {str(key+":").ljust(15)} {str(value)}')
 
                 if self.relay_socket in ready_sockets:
                     # Expects a request of the form: ['check/load', {'frame': int, '**kwargs': value}]
                     print(f'**** RELAY: {time.strftime("%H:%M:%S", time.localtime())}, {time.time() - self.t0} seconds')
                     request = self.relay_socket.recv_json()
                     print(f'request = {request}')
+                    ### ToDo: Change "check_energy"-request to "initialize"-request and implement params for cropping, rebinning, background subtract
+                    #!#
+                    if request[0] == 'preprocess':
+                        self.init_params = request[1]
+                        self.relay_socket.send_json(['Preprocess message received'])
+                        self.do_crop = 'shape' in self.init_params.keys()
+                        self.do_rebin = 'rebin' in self.init_params.keys()
+                        #!# self.do_rebin = ...
+
+                    #!#
                     if request[0] == 'check_energy':
                         print('check_energy')
                         if not self.energy_replied and self.Energy != None:
-                            self.relay_socket.send_json(
-                                    {'energy': float(self.Energy), 'self.latest_pos_index_received': int(self.latest_pos_index_received),
-                                     'self.latest_det_index_received': int(self.latest_det_index_received)})
+                            self.relay_socket.send_json({'energy': float(self.Energy)})
                             self.energy_replied = True
                             self.nr_of_check_replies += 1
                         else:
@@ -156,15 +163,8 @@ class RelayServer(object):
                     elif request[0] == 'check':
                         print('check')
                         print('inside REQ2')  ### DEBUG
-                        # nr of images:
-                        if len(self.recieved_image_indices) != self.latest_det_index_received + 1:  ## "+1" Because counting starts on 0
-                            im_acc = len(self.recieved_image_indices)
-                            print('Some diffraction pattern(s) got lost!!')  ## Maybe raise a warning?
-                        else:
-                            im_acc = self.latest_det_index_received + 1
-                        # nr of positions:
-                        frames_accessible_tot = min(im_acc,
-                                                       self.latest_pos_index_received + 1)  # Just sending total nr of frames accessible, even if some of them have already been sent
+                        frames_accessible = list(set(list(self.all_msg.keys())).intersection(list(self.all_img.keys())) )
+                        frames_accessible_tot = len(frames_accessible) #!# ToDo: LS MUST BE UPDATED: Now sending nr of NEW frames accessible!!!
                         print(f'sending [int(frames_accessible_tot), self.end_of_scan and self.end_of_det_stream] = {[int(frames_accessible_tot), self.end_of_scan and self.end_of_det_stream]}') ### DEBUG
                         self.relay_socket.send_json([int(frames_accessible_tot), self.end_of_scan and self.end_of_det_stream])
                         self.nr_of_check_replies += 1
@@ -172,14 +172,33 @@ class RelayServer(object):
                         print('load')
                         self.frame_nr = request[1]['frame']
                         # Get the correct indices corresponding to the requested frame_nr
-                        frame_msg_ind = np.array(self.recieved_pos_indices)[self.frame_nr]
-                        frame_img_ind = np.array(self.recieved_image_indices)[self.frame_nr]
-                        sendmsg = np.array(self.all_msg)[frame_msg_ind]
-                        sendimg = np.array(self.all_img)[frame_img_ind]
+                        sendmsg = [self.all_msg.pop(key) for key in self.frame_nr]
+                        #!###sendimg = np.array([self.crop(self.all_img.get(key)) for key in self.frame_nr]) #!# CHANGE get TO POP!
+                        sendimg = np.array([self.all_img.get(key) for key in self.frame_nr])  # !# CHANGE get TO POP!
+                        if self.do_crop:
+                            sendimg, self.newcenter, self.padmask = self.crop(sendimg)
+                            if self.load_replies == 0:
+                                sendmsg[0]['new_center'] = np.array(self.newcenter)
+                        if self.do_rebin:
+                            try:
+                                weight = np.ones_like(sendimg)
+                                weight[np.where(sendimg == 2 ** 32 - 1)] = 0
+                                weight = u.rebin_2d(weight, self.init_params['rebin'])
+                                sendimg = u.rebin_2d(sendimg, self.init_params['rebin'])
+                                sendimg = np.array([sendimg, weight])
+                                sendmsg[0]['RS_rebinned'] = True
+                                if self.newcenter is not None:
+                                    sendmsg[0]['new_center'] = np.array(self.newcenter) / float(self.init_params['rebin'])
+                            except:
+                                print('Warning: could not rebin, leaving this task to PtyPy instead.')
+                                sendmsg[0]['RS_rebinned'] = False
+
+                        self.sendimg.append(sendimg)  # !# DEBUG, remove line later
                         sendmsg[0]['dtype'] = sendimg[0].dtype  # Used for decompressing image in LS
                         sendmsg[0]['shape'] = sendimg.shape  # Used for decompressing image in LS
                         self.relay_socket.send_pyobj(sendmsg, flags=zmq.SNDMORE)
                         self.relay_socket.send(compress_lz4(sendimg), copy=True)
+                        self.load_replies += 1
                     elif request[0] == 'stop':
                         self.relay_socket.send_json(['closing connection to relay_socket'])
                         self.stop_outstream()
@@ -205,13 +224,130 @@ class RelayServer(object):
         print(f'Closing self.det_socket, self.pos_socket! at time {time.strftime("%H:%M:%S", time.localtime())}')
         self.det_socket.close()
         self.pos_socket.close()
-        print(f'self.recieved_pos_indices[-1] = {self.recieved_pos_indices[-1]}, self.recieved_image_indices[-1] = {self.recieved_image_indices[-1]}')
-
 
     def stop_outstream(self):
         print(f'Closing the relay_socket at {time.strftime("%H:%M:%S", time.localtime())}! {time.time()-self.t0} seconds')
         self.relay_socket.close()
         self.running = False
+
+    def crop(self, diff, get_weights=True):
+        """
+        Crops a single diffraction pattern of type ndarray.
+
+        Padding to make sure that diffraction center is in the image center
+        is yet not implemented, but performed in ptypy.
+
+        Parameters
+        ----------
+        diff
+        get_weights
+
+        Returns
+        -------
+
+        """
+
+        # Only allow square slices in data
+        self.init_params['shape'] = np.min(self.init_params['shape'])
+
+        # a: desired crop shape, b: resulting crop shape, c: center , d: diff shape
+
+        if self.center is None:
+            ##if 'center' in self.init_params.keys() and not isinstance(self.init_params['center'], str):
+            if 'center' in self.init_params.keys() and  isinstance(self.init_params['center'], (list, tuple)):
+                c_d = np.array(self.init_params['center'])  ## cy, cx : row, col  ### [751, 343]
+            else:
+                c_d = np.rint(self.find_center('all', 'auto')).astype(int)
+            self.center = c_d
+        else:
+            c_d = self.center
+
+        # Find limits for cropping
+        d_sh = diff.shape[-2:]  # (rows, cols) (yspan, xspan)
+        a_sh = int(self.init_params['shape'])
+
+        a_lowlims = c_d - a_sh // 2  # !##  array([367, -41])
+        a_highlims = c_d + (a_sh + 1) // 2  # (a_sh + 1) to enable uneven nr of pixels.
+
+        # Set the limits within the boundaries of diffraction pattern
+        b_lowlims = np.max((a_lowlims, np.zeros(2, dtype='int')), axis=0)
+        b_highlims = np.min((a_highlims, np.array(d_sh)), axis=0)
+        lims = np.array([b_lowlims, b_highlims])
+
+        ##diff_crop = diff[b_lowlims[0]:b_highlims[0], b_lowlims[1]:b_highlims[1]]
+        diff_crop = np.ascontiguousarray(np.split(np.split(diff, lims[:, 0], axis=-2)[1], lims[:, 1], axis=-1)[1]) # using ascontiguousarray automatically means that you'll make a copy so the array can be stored contiguously.
+
+
+        c_a = np.array([a_sh//2, a_sh//2])
+        c_b = tuple(c_d - b_lowlims)  # center w.r.t cropped diffraction pattern
+
+        # Adding padding
+        padlims = np.array([b_lowlims - a_lowlims, a_highlims - b_highlims])
+        padwidth = np.zeros((len(diff.shape), 2), dtype='int64')  # Used to get the correct shape for padwidth in np.pad
+        padwidth[-2:, :] = np.array([(padlims[:, 0], padlims[:, 1])])
+        diff_croppad = np.pad(diff_crop, padwidth)
+        ####### TODO: INCLUDE PADDING INTO THE WEIGHTS!!!! + update LS now that cropping always works (no need for checking this)
+        if get_weights:
+            w = np.ones_like(diff_crop)
+            w[np.where(diff_crop == 2 ** 32 - 1)] = 0
+            w = np.pad(w, padwidth)
+
+        return diff_croppad, c_a, padwidth
+        #!#return diff_crop, c_b
+
+    def find_center(self, diff, mask=None):
+        """
+        Finds the center of mass in the diffraction patterns.
+        Center is calculated using all diffraction patterns at the same time,
+        meaning that if one diffraction pattern would have half of the intensity
+        of a second diffraction pattern, then it will also contribute half as
+        much, compared to the second one.
+        ToDo: make all contribute equally
+
+        Parameters
+        ----------
+        diff : ##dict of 2D ndarrays
+        mask : 2D ndarray
+
+        Returns
+        -------
+
+        """
+        if isinstance(diff, str):
+            diff = self.all_img
+        diff = np.array([diff[key] for key in diff.keys()])
+        axes = tuple(range(1, diff.ndim + 1))
+
+        if mask == 'auto':
+            mask = np.ones_like(diff[0])
+            mask[np.where(diff[0] == 2 ** 32 - 1)] = 0
+
+        if mask is None:
+            return (np.sum(diff * np.indices(diff.shape), axis=axes, dtype=float) / np.sum(diff, dtype=float))[-2:]
+        else:
+            return (np.sum(diff * mask * np.indices(diff.shape), axis=axes, dtype=float) / np.sum(diff * mask, dtype=float))[-2:]
+
+# In progress:
+    # def rebin(self, diff, return_weights=True):
+    #     w = np.ones_like(diff_crop)
+    #     ## padwidth: (padrows_start, padrows_end), (padcols_start, padcols_ebd)
+    #     w[:, :, ()]
+    #     np.pad(w, padwidth//2)
+    #
+    #     weight = np.ones_like(sendimg)
+    #     weight[np.where(sendimg == 2 ** 32 - 1)] = 0
+    #     weight = u.rebin_2d(weight, self.init_params['rebin'])
+    #
+    #     sendimg = u.rebin_2d(sendimg, self.init_params['rebin'])
+    #     sendimg = np.array([sendimg, weight])
+    #     sendmsg[0]['RS_rebinned'] = True
+    #     if return_weights:
+    #
+    #         return diff
+    #     else:
+    #         return diff
+
+
 
 
 if __name__ == "__main__":
